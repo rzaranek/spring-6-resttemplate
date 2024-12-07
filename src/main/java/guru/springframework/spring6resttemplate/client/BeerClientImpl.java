333f3d683fe8ec5a3784632db5c3d54d4dbf2874
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-12-07 15:15:49 +0100

    get list as a map

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -8,9 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
+
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String GET_BEER_PATH = "/api/v1/beer";
 
     private final RestTemplateBuilder restTemplateBuilder;
 
@@ -20,7 +25,10 @@ public class BeerClientImpl implements BeerClient {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
         ResponseEntity<String> stringResponseEntity =
-            restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+            restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+
+        ResponseEntity<Map> mapResponseEntity =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
 
         System.out.println(stringResponseEntity.getBody());
 
