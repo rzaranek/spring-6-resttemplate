4d5d3ea757bfece95c7e60867e475d5c982f0e5f
Robert Zaranek <robert.zaranek@bilansjr.pl>
2024-12-07 15:03:53 +0100

    get list as string

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,11 +1,29 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
+
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<String> stringResponseEntity =
+            restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+
+        System.out.println(stringResponseEntity.getBody());
+
         return null;
     }
 }
